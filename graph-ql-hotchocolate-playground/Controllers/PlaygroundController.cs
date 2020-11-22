f07b098ee69321ed40023a98eb441ac60fd39b22
bnaya eshet <bnayanet@hotmail.com>
2020-11-22 17:48:44 +0200

    Add project files.

---
+++ graph-ql-hotchocolate-playground/Controllers/PlaygroundController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bnaya.Samples.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PlaygroundController : ControllerBase
+    {
+        private readonly ILogger<PlaygroundController> _logger;
+
+        public PlaygroundController(ILogger<PlaygroundController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
---
+++ graph-ql-hotchocolate-playground/GraphQL/Query.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bnaya.Samples
+{
+    public class Query
+    {
+        public string Hello() => "world";
+    }
+}
